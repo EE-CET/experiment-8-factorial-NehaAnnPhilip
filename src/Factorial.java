9ed5b086255127b8f22178f3bbf8c8958a989ac4
NehaAnnPhilip <nehaphilip0205@gmail.com>
2026-03-25 11:46:43 +0530

    Completed Factorial

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,11 +1,30 @@
+
+import java.util.Scanner;
+
 public class Factorial {
-
-    // TODO: Create a method 'public static long factorial(int n)'
-    // It should return the factorial of n.
-
-    
-        // TODO: Read input n
-        // TODO: Call factorial(n)
-        // TODO: Print the result
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        boolean isPrime = true;
+        
+        if (n <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        
+        if (isPrime) {
+            System.out.println("Prime");
+        } else {
+            System.out.println("Not Prime");
+        }
+        
+        sc.close();
+    }
 }
